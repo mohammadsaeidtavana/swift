9307ed32ba0586815fb7ca5c966824e6ac225b55
mohammad saeid tavana <36128128+mohammadsaeidtavana@users.noreply.github.com>
2018-08-10 18:09:06 +0400

    Add files via upload

---
+++ mywatch/mywatch WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  mywatch WatchKit Extension
+//
+//  Created by Saeed Tavana on 8/10/18.
+//  Copyright © 2018 Saeed Tavana. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
